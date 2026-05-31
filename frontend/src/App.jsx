0a9fe405cdc1fd501bcddc073cc67b39696abe2d
Софья <sofypavlova2005@gmail.com>
2026-05-31 14:06:17 +0300

    feat(frontend): создать React-проект и настроить базовую структуру приложения

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+
+export default function App() {
+
+  return (
+    <>
+    </>
+  );
+}
